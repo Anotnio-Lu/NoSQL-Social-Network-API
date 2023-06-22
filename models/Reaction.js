eb30476f3ee316849ea8c6c69717bcb6db93ddf1
Antonio lu <antonioraylu@outlook.com>
2023-06-22 14:51:50 +1000

    Created reaction, thought and user model with a helper js file that exports a dateformat function

---
+++ models/Reaction.js
@@ -0,0 +1,33 @@
+const { Schema, Types } = require('mongoose');
+const helpers = require('../utils/helpers');
+
+const reactionSchema = new Schema(
+  {
+    reactionId: {
+      type: Schema.Types.ObjectId,
+      default: () => new Types.ObjectId(),
+    },
+    reactionBody: {
+      type: String,
+      required: true,
+      maxlength: 280
+    },
+    username: {
+      type: String,
+      required: true
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: (timestamp) => helpers.dateFormat(timestamp) 
+    }
+  },
+  {
+    toJSON: {
+      getters: true,
+    },
+    id: false,
+  }
+);
+
+module.exports = reactionSchema;
