3b21eaa69f61918e144fae43d0dcc798b192aebd
Antonio lu <antonioraylu@outlook.com>
2023-06-22 15:17:58 +1000

    Created post to create user and get user by id

--- controllers/userController.js
+++ controllers/userController.js
@@ -7,5 +7,22 @@ module.exports = {
       .then((users) => res.json(users))
       .catch((err) => res.status(500).json(err));
   },
+  getSingleUser(req, res) {
+    User.findOne({ _id: req.params.userId })
+      .select('-__v')
+      .then((user) =>
+        !user
+          ? res.status(404).json({ message: 'No user with that ID' })
+          : res.json(user)
+      )
+      .catch((err) => res.status(500).json(err));
+  },
+  createUser(req, res) {
+    User.create(req.body)
+      .select('-__v')
+      .then((dbUserData) => res.json(dbUserData))
+      .catch((err) => res.status(500).json(err));
+  },
 
+  
 };
