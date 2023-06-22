29844d1a9820319ff3ae48e9ad5e9923b9089b69
Antonio lu <antonioraylu@outlook.com>
2023-06-22 15:24:07 +1000

    Created put request to update thought and delete request to remove thought

--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -3,14 +3,19 @@ const {
   getThoughts,
   getSingleThought,
   createThought,
-
+  updateThought,
+  deleteThought,
+  addThoughtIdReaction,
+  removeThoughtReaction,
 } = require('../../controllers/thoughtController');
 
 // /api/thoughts
 router.route('/').get(getThoughts).post(createThought);
 
 // /api/thoughts/:thoughtId
-router.route('/:thoughtId').get(getSingleThought)
+router.route('/:thoughtId').get(getSingleThought).put(updateThought).delete(deleteThought);
+
+
 
 
 module.exports = router;
